39eac8989ec6bf8126eeee8ec52fc5818e514d8f
tom-stringer <tomstringer99@gmail.com>
2024-07-25 18:01:20 +0100

    Clean existing zip at start of build script

--- build.ps1
+++ build.ps1
@@ -1,2 +1,9 @@
-New-Item -Path dist -ItemType Directory -Force
-&"C:\Program Files\7-Zip\7z.exe" a -tzip -y .\dist\Goofycraft.zip .\src\*
+$BuildDirectory = "dist"
+$BuildZip = "$BuildDirectory\Goofycraft.zip"
+$7ZipExe = "C:\Program Files\7-Zip\7z.exe"
+
+if (Test-Path $BuildDirectory) {
+    Remove-Item -Path $BuildDirectory -Recurse -Force
+}
+New-Item -Path $BuildDirectory -ItemType Directory -Force
+&$7ZipExe a -tzip -y $BuildZip .\src\*
