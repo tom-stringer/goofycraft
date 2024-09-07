3aeb5020e156b483ba41bd3074d9238a2fa5bd37
tom-stringer <tomstringer99@gmail.com>
2024-09-07 10:01:00 +0100

    Move assets to resources

--- src/main/java/github/tomstringer/goofycraft/Main.java
+++ src/main/java/github/tomstringer/goofycraft/Main.java
@@ -7,7 +7,7 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        Path soundsPath = Path.of("src/assets/minecraft/sounds.json");
+        Path soundsPath = Path.of("src/main/resources/assets/minecraft/sounds.json");
 
         new SoundsWriter(new SoundsFactory(), soundsPath)
                 .writeSoundsFile();
